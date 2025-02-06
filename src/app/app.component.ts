f0725c7c43f1d7097b7a9e6e69590fc90acb82d2
Angelo Lima Dias <angelo@techdog.com.br>
2025-02-06 08:57:33 -0300

    comecando projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'pokedex';
+  pokemons: any = [];
+  constructor(private http: HttpClient) {
+    this.obterPokemons();
+  }
+
+  obterPokemons() {
+    this.http
+      .get('https://pokeapi.co/api/v2/pokemon')
+      .subscribe((response: any) => {
+        response.results.map((pokemon: any) => {
+          this.http.get(pokemon.url).subscribe((pokemonData: any) => {
+            this.pokemons.push(pokemonData);
+            console.log(this.pokemons);
+          })
+          ;
+        });
+      });
+      // this.pokemons = response.results;
+  }
 }
